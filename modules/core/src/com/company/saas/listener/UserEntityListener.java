a60928f5a2b562738191e21c73d15a6db001cd2f
Natalya Firstova <firstova@haulmont.com>
2016-10-26 15:01:09 +0400

    Upgrade to platform-6.3.2

--- modules/core/src/com/company/saas/listener/UserEntityListener.java
+++ modules/core/src/com/company/saas/listener/UserEntityListener.java
@@ -27,6 +27,7 @@ import com.haulmont.cuba.security.entity.User;
 
 import javax.inject.Inject;
 import java.util.List;
+import com.haulmont.cuba.core.EntityManager;
 
 @Component("saas_UserEntityListener")
 public class UserEntityListener implements BeforeInsertEntityListener<User> {
@@ -38,7 +39,7 @@ public class UserEntityListener implements BeforeInsertEntityListener<User> {
     private Metadata metadata;
 
     @Override
-    public void onBeforeInsert(User entity) {
+    public void onBeforeInsert(User entity, EntityManager entityManager) {
         System.out.println("saas_UserEntityListener.onBeforeInsert");
 
         TypedQuery<Role> query = persistence.getEntityManager().createQuery("select r from sec$Role r where r.defaultRole = true", Role.class);
